5f3b941e9cdf8c25ffdc4f5d7e11d046b885d81e
Harsha Bharadwaj <88173012+harsha-bharadwaj100@users.noreply.github.com>
2023-10-08 14:26:19 +0530

    CSS, JS dynamic star rating!

--- script.js
+++ script.js
@@ -3,15 +3,8 @@ starer = (rating) => {
     for (let i = 0; i < document.getElementsByClassName("star").length; i++) {
         document.getElementsByClassName("star")[i].classList.remove("stared");
     }
-    // for (let i = 0; i < document.getElementsByClassName("star").length; i++) {
-    //     document.getElementsByClassName("star")[i].classList.remove("unstared");
-    // }
     for (let i = 0; i < rating; i++) {
         document.getElementsByClassName("star")[i].classList.add("stared");
         console.log("added stared");
     }
-    // for (let i = rating+1; i < document.getElementsByClassName("star").length - rating; i++) {
-    //     document.getElementsByClassName("star")[i].classList.add("unstared");
-    //     console.log("added unstared");
-    // }
 }
